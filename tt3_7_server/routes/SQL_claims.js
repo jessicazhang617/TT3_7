6d9dd3ccc0b83c7a6f06b7d1b01c503ab13e2147
ChooJiaXin <e0426255@u.nus.edu>
2023-05-06 14:03:52 +0800

    Create claims router

--- tt3_7_server/routes/SQL_claims.js
+++ tt3_7_server/routes/SQL_claims.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import { } from '../controllers/...';
+import { } from '../controllers/SQL_claims.js';
 
 const router=express.Router(); 
 
-router.get(); 
-router.get();
-router.get();
-router.get(); 
+router.get('/approved/:employeeID'); 
+router.get('/pending/:employeeID');
+router.get('/rejected/:employeeID');
 
-router.post(); 
-router.delete();
-router.add();
+router.post('/edit/'); 
+router.post('add/')
+router.delete('/delete/:claimID');
 
 
